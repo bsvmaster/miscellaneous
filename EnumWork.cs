80c95b72d664a7c75e6ec4d01529057d27f73e5d
bsvmaster <eltoro@ukr.net>
2016-04-08 10:14:15 +0300

    Update EnumWork.cs

--- EnumWork.cs
+++ EnumWork.cs
@@ -1,7 +1,8 @@
 namespace EnumExtensions
 public static class EnumExtensions
 {
-	public static Dictionary<int, string> GetEnumList<T>() where T : struct, IConvertible // return Dictionary <value, Name> from all items of Enum type
+  // return Dictionary <value, Name> from all items of Enum type
+  public static Dictionary<int, string> GetEnumDictionary<T>() where T : struct, IConvertible 
   {
 		if (!typeof(T).IsEnum)
 			throw new Exception("enums only!");
@@ -15,8 +16,25 @@ public static class EnumExtensions
         }
         return  items;
   }
+  
+  // return List of SelectListItems from Enum
+   public static List<System.Web.Mvc.SelectListItem> GetEnumSelectedList<T>() where T : struct, IConvertible //
+   {
+		if (!typeof(T).IsEnum)
+			throw new Exception("enums only!");
+		
+        Array values = Enum.GetValues(typeof(T));
+        var items = new List<System.Web.Mvc.SelectListItem>();
+
+        foreach (var i in values)
+        {	 
+            items.Add(new System.Web.Mvc.SelectListItem{ Value = ((int)i).ToString(), Text = ( Enum.Parse(typeof(T), i.ToString()) as Enum ).Name()} );
+        }
+        return  items;
+   }
 	
-   public static string Name(this Enum instance) //if Enum has an attribute Display Name= return it, else return name of item
+   //if Enum has an attribute Display Name= return it, else return name of item
+   public static string Name(this Enum instance) 
    {
         string name = instance.ToString();
         MemberInfo field = instance.GetType().GetField(name);
@@ -30,3 +48,10 @@ public static class EnumExtensions
         return name;
    }
 }
+
+// -------- *** Using in ASP.NET MVC *** ---------
+// in Controller:
+// ViewBag.List = EnumExtensions.GetEnumSelectedList<MyEnumType>();
+
+// in View:
+// @Html.DropDownList("FieldName", (IEnumerable<SelectListItem>)ViewBag.List, "--- Chose item ---", null) 
